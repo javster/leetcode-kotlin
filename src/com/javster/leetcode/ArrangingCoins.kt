41cacb126b5bff2a14d4e470b97ba7ece9dd85d3
anton.kuritsyn <anton.kuritsin@gmail.com>
2020-07-15 23:05:29 +0300

    ReverseWords and ArrangingCoins

---
+++ src/com/javster/leetcode/ArrangingCoins.kt
@@ -0,0 +1,25 @@
+package com.javster.leetcode
+
+fun main() {
+    println(ArrangingCoins().arrangeCoins(3))
+    println(ArrangingCoins().arrangeCoins(5))
+    println(ArrangingCoins().arrangeCoins(8))
+}
+
+class ArrangingCoins {
+    fun arrangeCoins(n: Int): Int {
+        var row = 0
+        var total = n
+        do {
+            val res = substractCoins(row, total)
+            total = res.second
+            if (res.first) row++
+        } while (res.first)
+        return row
+    }
+
+    fun substractCoins(row: Int, total: Int): Pair<Boolean, Int> {
+        if (total < row + 1) return Pair(false, 0)
+        else return Pair(true, total - (row + 1))
+    }
+}
